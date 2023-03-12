631974c4b0d707944163bf627b80688932f718ef
fazalshah52 <bilalfazal52@gmail.com>
2023-03-12 09:10:14 -0700

    pending state

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+        <div className="logo">LOGO</div>
+        <span>Home</span>
+        <span>products</span>
+        <span>add product</span>
+        <button>login</button>
+        <button>signup</button>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
